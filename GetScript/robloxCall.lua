16b62e18b22a330b91d684cad5f2f450b88d0702
ZoiIntra <94178333+ZoiIntra@users.noreply.github.com>
2022-12-25 20:21:41 +0700

    Update robloxCall.lua

--- GetScript/robloxCall.lua
+++ GetScript/robloxCall.lua
@@ -4,6 +4,7 @@ LOCAL HUB V2
 Local Hub Power By Zoi King Community!
 
 ]]--
-getgenv().key = "" -- If you have key
-getgenv().namehub = "" -- Skip choose hub ui put support hub here ( Local / Mukuro / MukuroOld / Hoho / ThunderZ )
+getgenv().key = "" -- ถ้าคุณมี Key
+getgenv().namehub = "" -- ใส่ชื่อค่ายที่รองรับตรงนี้เช่น Local / Mukuro / MukuroOld / Hoho / ThunderZ เป็นต้น
+getgenv().device = "" -- ใส่อุปกรณ์ของคุณเช่น PC / Mobile เท่านั้น
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ZoiIntra/BloxFruit/main/Local.lua'))()
