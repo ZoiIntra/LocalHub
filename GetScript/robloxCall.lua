9d65185240050e4712040db44d0468cf5e307959
ZoiIntra <94178333+ZoiIntra@users.noreply.github.com>
2023-01-04 12:25:53 +0700

    Update robloxCall.lua

--- GetScript/robloxCall.lua
+++ GetScript/robloxCall.lua
@@ -4,5 +4,5 @@ LOCAL HUB V3
 Local Hub Power By Zoi King Community!
 
 ]]--
-getgenv().key = "" -- ถ้าคุณมี Key / if u have key
+_G.key = "" -- ถ้าคุณมี Key / if u have key
 loadstring(game:HttpGet('https://raw.githubusercontent.com/ZoiIntra/BloxFruit/main/Local.lua'))()
